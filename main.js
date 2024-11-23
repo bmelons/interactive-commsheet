b3f3076ad3c69ab5be6748b471c5e0bb16b9a414
bmelons <andrewsgowan@gmail.com>
2024-11-22 19:07:34 -0600

    soooo much

--- main.js
+++ main.js
@@ -1,24 +1,32 @@
 
-function fetchJSONData() {
-    fetch("./options.json")
-    .then((res) => {
-        if (!res.ok) {
-            throw new Error
-            (`HTTP error! Status: ${res.status}`);
-        }
-        return res.json();
-    })
-    .then((data) =>
-        console.log(data))
-    .catch((error) =>
-        console.error("Unable to fetch data:", error));
-}
-let awesome = fetchJSONData();
+let awesome = [
+    {
+        "name": "Stage",
+        "desc": "how quickly?",
+        "options": [
+            {
+                "option": "Sketched",
+                "price": 8
+            },
+            {
+                "option": "Colored Sketch",
+                "price": 12
+            },
+            {
+                "option": "Inked Flats",
+                "price": 18
+            },
+            {
+                "option": "Shaded",
+                "price": 24
+            }
+        ]
+    }
+]
 
 
-function load() 
-{
-    console.log(awesome)
+function load() {
+    // console.log(awesome)
     let s = document.getElementById("basePreview")
     let ls = document.getElementById("PreviewA")
     let __factor = document.getElementById("factor1")
@@ -26,24 +34,38 @@ function load()
     let listTemplate = __factor.cloneNode(true)
 
     __factor.remove()
-    function makeOption(factor,id) {
+    function makeOption(factor, id, price) {
         let clone = s.cloneNode(true)
         // console.log(clone.childNodes[1].childNodes[3].textContent)
-        clone.childNodes[1].childNodes[3].textContent = id
+        clone.childNodes[1].childNodes[5].textContent = id;
+        clone.childNodes[1].childNodes[7].textContent = "$" + price.toString();
         factor.childNodes[7].appendChild(clone)
-        
+        clone.childNodes[3].addEventListener('click', function () {
+            let pair = clone.childNodes[1]
+            let radio = pair.childNodes[1]
+            radio.checked = true
+            console.log(pair.childNodes)
+        })
         // console.log("OPTIEN MADE")
     }
-    function makeList(id,desc) { //1,3
+    function makeList(id, desc) { //1,3
         let newList = listTemplate.cloneNode(true);
         newList.id = id
-        console.log(newList.childNodes)
+        // console.log(newList.childNodes)
         newList.childNodes[1].textContent = id
         newList.childNodes[3].textContent = desc
         return document.body.appendChild(newList);
     }
-    let n = makeList("apple","radda radda");
-    makeOption(n,"a")
+    for (let f = 0; f < awesome.length; f++) {
+        let id = awesome[f].name
+        let desc = awesome[f].desc
+        let contents = awesome[f].options
+        let n = makeList(id, desc);
+        for (let i = 0; i < contents.length; i++) {
+            let current = contents[i]
+            makeOption(n, current.option, current.price)
+        }
+    }
 
     // for (let i = 1; i<=4; i++) {
     //     let clone = s.cloneNode(true)
@@ -56,8 +78,8 @@ function load()
     //         console.log(pair.childNodes)
     //     })
     // }
-    
-    
+
+
 }
 
 window.onload = load
